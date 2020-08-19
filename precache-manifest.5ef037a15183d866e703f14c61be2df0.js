4d236469945b822376ac0b36981a29adb3cacbd5
Alvaro Serrano <alsersan20@gmail.com>
2020-08-19 23:46:34 +0200

    Updates

---
+++ precache-manifest.5ef037a15183d866e703f14c61be2df0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51235cc0a9bbe4013dd0d6e8ee1a412a",
+    "url": "/arc-development/index.html"
+  },
+  {
+    "revision": "ab409dee5da3e24102d3",
+    "url": "/arc-development/static/js/2.8b9dc4e9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/arc-development/static/js/2.8b9dc4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0635d03132a1f50dc9a",
+    "url": "/arc-development/static/js/main.7a3fb832.chunk.js"
+  },
+  {
+    "revision": "24ddae678c464ff103fe",
+    "url": "/arc-development/static/js/runtime-main.bd6f039d.js"
+  },
+  {
+    "revision": "e227d87b2dec28a248985906d655c9ed",
+    "url": "/arc-development/static/media/Footer Adornment.e227d87b.svg"
+  },
+  {
+    "revision": "73caaa01e4d700f4b23e47665ecc2727",
+    "url": "/arc-development/static/media/facebook.73caaa01.svg"
+  },
+  {
+    "revision": "0b117993cfdc06f6a05973ffa37ab7dc",
+    "url": "/arc-development/static/media/instagram.0b117993.svg"
+  },
+  {
+    "revision": "57d64f65026b8edaf73e17d8a41e6f23",
+    "url": "/arc-development/static/media/logo.57d64f65.svg"
+  },
+  {
+    "revision": "435fc4bc44c1e90d5f00f4efccdb9b1d",
+    "url": "/arc-development/static/media/twitter.435fc4bc.svg"
+  }
+]);
