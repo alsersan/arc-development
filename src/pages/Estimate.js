a4dd25b850587a23d691606c4d26afda92537f24
Alvaro Serrano <alsersan20@gmail.com>
2020-09-27 16:44:07 +0200

    modifications on Estimate page

--- src/pages/Estimate.js
+++ src/pages/Estimate.js
@@ -10,8 +10,9 @@ import useMediaQuery from "@material-ui/core/useMediaQuery";
 import check from "../assets/check.svg";
 import send from "../assets/send.svg";
 import software from "../assets/software.svg";
-import mobile from "../assets/mobile.svg";
-import website from "../assets/website.svg";
+import customSoftwareIcon from "../assets/Custom Software Icon.svg";
+import appDevelopmentIcon from "../assets/mobileIcon.svg";
+import websiteIcon from "../assets/websiteIcon.svg";
 import backArrow from "../assets/backArrow.svg";
 import forwardArrow from "../assets/forwardArrow.svg";
 import backArrowDisabled from "../assets/backArrowDisabled.svg";
@@ -34,9 +35,38 @@ import biometrics from "../assets/biometrics.svg";
 
 import estimateAnimation from "../animations/estimateAnimation/data.json";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  sectionContainer: {
+    padding: "3rem 1rem 4rem",
+  },
+  animation: {
+    maxWidth: "50rem",
+    marginTop: "4rem",
+  },
+  optionsContainer: {
+    padding: "3rem 1rem 4rem",
+    textAlign: "center",
+  },
+  optionsHeading: {
+    margin: "4rem 0",
+    fontWeight: 700,
+  },
+  optionContainer: {
+    padding: " 0.5rem",
+  },
+  optionHeading: {
+    maxWidth: "13rem",
+    marginBottom: "1.5rem",
+    fontWeight: 600,
+  },
+  image: {
+    height: "10rem",
+  },
+}));
 
 const Estimate = () => {
+  const classes = useStyles();
+
   const defaultOptions = {
     loop: true,
     autoplay: false,
@@ -48,15 +78,98 @@ const Estimate = () => {
 
   return (
     <Grid container>
-      <Grid item container direction="column">
+      <Grid
+        item
+        container
+        direction="column"
+        lg={5}
+        className={classes.sectionContainer}
+      >
         <Grid item>
           <Typography variant="h2">Estimate</Typography>
         </Grid>
-        <Grid item>
+        <Grid item className={classes.animation}>
           <Lottie options={defaultOptions} isStopped={true} />
         </Grid>
       </Grid>
-      <Grid item container direction="column"></Grid>
+      <Grid
+        item
+        container
+        direction="column"
+        lg={7}
+        className={classes.optionsContainer}
+      >
+        <Grid item>
+          <Typography variant="h4" className={classes.optionsHeading}>
+            Which service are you interested in?
+          </Typography>
+        </Grid>
+        <Grid item container>
+          <Grid
+            item
+            container
+            direction="column"
+            md
+            alignItems="center"
+            className={classes.optionContainer}
+          >
+            <Grid item>
+              <Typography variant="h6" className={classes.optionHeading}>
+                Custom Software Development
+              </Typography>
+            </Grid>
+            <Grid item>
+              <img
+                src={customSoftwareIcon}
+                alt="three floating screens"
+                className={classes.image}
+              />
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            container
+            direction="column"
+            md
+            alignItems="center"
+            className={classes.optionContainer}
+          >
+            <Grid item>
+              <Typography variant="h6" className={classes.optionHeading}>
+                iOS/Android App Development
+              </Typography>
+            </Grid>
+            <Grid item>
+              <img
+                src={appDevelopmentIcon}
+                alt="phones and tablet outline"
+                className={classes.image}
+              />
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            container
+            direction="column"
+            md
+            alignItems="center"
+            className={classes.optionContainer}
+          >
+            <Grid item>
+              <Typography variant="h6" className={classes.optionHeading}>
+                Website Development
+              </Typography>
+            </Grid>
+            <Grid item>
+              <img
+                src={websiteIcon}
+                alt="website outline"
+                className={classes.image}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
